991f3284d87fda1e8d857d1153901c30096149aa
Jacob Gadikian <jacobgadikian@gmail.com>
2021-09-07 03:42:36 +0700

    Fix first of two changes to transaction factory

--- relayer/chain.go
+++ relayer/chain.go
@@ -388,7 +388,7 @@ type protoTxProvider interface {
 // the encoded transaction or an error if the unsigned transaction cannot be
 // built.
 func BuildSimTx(txf tx.Factory, msgs ...sdk.Msg) ([]byte, error) {
-	txb, err := tx.BuildUnsignedTx(txf, msgs...)
+	txb, err := txf.BuildUnsignedTx(msgs...)
 	if err != nil {
 		return nil, err
 	}
